1b20d1aa464e9ccab0de1a330ce4bac0419291ed
leonardo oliveira <leonardo_olivesilva@icloud.com>
2023-01-24 16:25:49 -0800

    Method

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,46 @@
 {
     public class Program
     {
+        public static int Add(int numb1, int numb2)
+        {
+            return numb1 + numb2;
+
+        }
+
+        public static decimal Add(decimal numb1, decimal numb2)
+        {
+            return numb1 + numb2;
+        }
+
+        public static string Add(int numb1, int numb2, bool isTrue)
+        {
+            var sum = 0;
+            var response = "";
+            if (isTrue)
+            {
+                sum = numb1 + numb2;
+                if (sum == 1)
+                {
+                   response = $"{sum} dollar.";
+                }
+                else
+                {
+                    response = $"{sum} dollars.";
+                }
+            }
+            return response;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(Add(0, 1, true));
+
+
+            Console.WriteLine(Add(5, 7, true));
+
+
+            Console.WriteLine(Add(-6, 3, true));
+
         }
     }
 }
